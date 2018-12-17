c20ceb079c5d85bbffa7f99460f2b24550e54fdf
ccbuzzell <cbuzzell@risd.edu>
2018-12-17 08:47:33 -0500

    overhaul

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
 
     $( "#nav-page" ).append( "<li><a href='books.html'>Books</a></li><li><a href='searchhistory.html'>Loops</a></li><li><a href='motion.html'>Motion + Video</a></li><li><a href='haunt.html'>Haunt</a></li><li><a href='identity.html'>Identity + Web</a></li><li><a href='shop.html'>Copy Shop</a></li>" );
 
+    $( "#bottom-nav-books" ).append( "<a href='books.html'><p>&#8592;&#8592;&#8592; back to books</p></a>" );
 
 });
 
