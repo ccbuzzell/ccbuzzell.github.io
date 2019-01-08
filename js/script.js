1e8a9f795d299a8dde31498ad99acb39549c7bd1
ccbuzzell <cbuzzell@risd.edu>
2019-01-07 19:15:41 -0500

    smaller

--- js/script.js
+++ js/script.js
@@ -54,7 +54,35 @@ function BookLoad () {
 }
 
 
-
+function VidLoad () {
+    var imgs=images(2),
+        last;
+    function images(count){
+        var images=[];
+        for ( var i = 1; i <= count; i++ ) {
+            images.push("imgs/videos/vid" + i + ".gif");
+        }
+        return images;
+    }
+    function randomImg_m() {
+        var imgNum_m= Math.floor(Math.random() * imgs.length);
+        while (last===imgNum_m) { // make sure the image is not the same as the last!
+            imgNum_m = Math.floor(Math.random() * imgs.length);
+        }
+        last = imgNum_m;
+        return imgs[imgNum_m];
+    }
+    function loop() {
+        var aRandomImage = randomImg_m();
+        $("<img>").hide().appendTo("#catalogs").on("load", function(e) {
+            $("#catalogs img:last").fadeIn(0);
+            $("#catalogs img:first").fadeOut(0,function(){ $(this).remove()})
+        
+        }).attr("src",aRandomImage)
+    }
+    document.write("<img src='" + randomImg_m() + "'>");
+    //setInterval(loop, 2500) // every few seconds
+}
 
 
 function IdLoad () {
@@ -86,38 +114,5 @@ function IdLoad () {
     document.write("<img src='" + randomImg_m() + "'>");
     //setInterval(loop, 2500) // every few seconds
 }
-
-
-
-function VidLoad () {
-    var imgs=images(2),
-        last;
-    function images(count){
-        var images=[];
-        for ( var i = 1; i <= count; i++ ) {
-            images.push("imgs/videos/vid" + i + ".gif");
-        }
-        return images;
-    }
-    function randomImg_m() {
-        var imgNum_m= Math.floor(Math.random() * imgs.length);
-        while (last===imgNum_m) { // make sure the image is not the same as the last!
-            imgNum_m = Math.floor(Math.random() * imgs.length);
-        }
-        last = imgNum_m;
-        return imgs[imgNum_m];
-    }
-    function loop() {
-        var aRandomImage = randomImg_m();
-        $("<img>").hide().appendTo("#catalogs").on("load", function(e) {
-            $("#catalogs img:last").fadeIn(0);
-            $("#catalogs img:first").fadeOut(0,function(){ $(this).remove()})
-        
-        }).attr("src",aRandomImage)
-    }
-    document.write("<img src='" + randomImg_m() + "'>");
-    //setInterval(loop, 2500) // every few seconds
-}
-
 
 
